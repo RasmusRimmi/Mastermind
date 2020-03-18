64134a1e493e0c81d5eb76d4d8a997c478c45f54
Riina Backman <gr188369@gradia.fi>
2020-03-18 13:06:54 +0200

    Credits labelit liikkuu ja Päävalikkonappi

--- Mastermind/View/Credits.Designer.cs
+++ Mastermind/View/Credits.Designer.cs
@@ -33,8 +33,9 @@
             this.lbTitle = new System.Windows.Forms.Label();
             this.lbNimi2 = new System.Windows.Forms.Label();
             this.lbNimi4 = new System.Windows.Forms.Label();
-            this.lvNimi3 = new System.Windows.Forms.Label();
+            this.lbNimi3 = new System.Windows.Forms.Label();
             this.lbNimi1 = new System.Windows.Forms.Label();
+            this.btPaavalikko = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbSuunnittelu
@@ -42,9 +43,9 @@
             this.lbSuunnittelu.AutoSize = true;
             this.lbSuunnittelu.Font = new System.Drawing.Font("Mistral", 22F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
             this.lbSuunnittelu.ForeColor = System.Drawing.Color.White;
-            this.lbSuunnittelu.Location = new System.Drawing.Point(181, 50);
+            this.lbSuunnittelu.Location = new System.Drawing.Point(207, 76);
             this.lbSuunnittelu.Name = "lbSuunnittelu";
-            this.lbSuunnittelu.Size = new System.Drawing.Size(220, 55);
+            this.lbSuunnittelu.Size = new System.Drawing.Size(176, 44);
             this.lbSuunnittelu.TabIndex = 0;
             this.lbSuunnittelu.Text = "SUNNITTELU";
             // 
@@ -53,9 +54,9 @@
             this.lbToteutus.AutoSize = true;
             this.lbToteutus.Font = new System.Drawing.Font("Mistral", 22F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
             this.lbToteutus.ForeColor = System.Drawing.Color.White;
-            this.lbToteutus.Location = new System.Drawing.Point(181, 228);
+            this.lbToteutus.Location = new System.Drawing.Point(207, 254);
             this.lbToteutus.Name = "lbToteutus";
-            this.lbToteutus.Size = new System.Drawing.Size(181, 55);
+            this.lbToteutus.Size = new System.Drawing.Size(145, 44);
             this.lbToteutus.TabIndex = 1;
             this.lbToteutus.Text = "TOTEUTUS";
             // 
@@ -64,9 +65,9 @@
             this.lbTitle.AutoSize = true;
             this.lbTitle.Font = new System.Drawing.Font("Mistral", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
             this.lbTitle.ForeColor = System.Drawing.Color.White;
-            this.lbTitle.Location = new System.Drawing.Point(270, 366);
+            this.lbTitle.Location = new System.Drawing.Point(296, 392);
             this.lbTitle.Name = "lbTitle";
-            this.lbTitle.Size = new System.Drawing.Size(316, 75);
+            this.lbTitle.Size = new System.Drawing.Size(253, 60);
             this.lbTitle.TabIndex = 2;
             this.lbTitle.Text = "MASTERMIND";
             // 
@@ -75,7 +76,7 @@
             this.lbNimi2.AutoSize = true;
             this.lbNimi2.Font = new System.Drawing.Font("Mistral", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
             this.lbNimi2.ForeColor = System.Drawing.Color.White;
-            this.lbNimi2.Location = new System.Drawing.Point(446, 85);
+            this.lbNimi2.Location = new System.Drawing.Point(472, 111);
             this.lbNimi2.Name = "lbNimi2";
             this.lbNimi2.Size = new System.Drawing.Size(79, 35);
             this.lbNimi2.TabIndex = 3;
@@ -86,42 +87,60 @@
             this.lbNimi4.AutoSize = true;
             this.lbNimi4.Font = new System.Drawing.Font("Mistral", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
             this.lbNimi4.ForeColor = System.Drawing.Color.White;
-            this.lbNimi4.Location = new System.Drawing.Point(448, 263);
+            this.lbNimi4.Location = new System.Drawing.Point(474, 289);
             this.lbNimi4.Name = "lbNimi4";
             this.lbNimi4.Size = new System.Drawing.Size(79, 35);
             this.lbNimi4.TabIndex = 4;
             this.lbNimi4.Text = "RIINA";
             // 
-            // lvNimi3
+            // lbNimi3
             // 
-            this.lvNimi3.AutoSize = true;
-            this.lvNimi3.Font = new System.Drawing.Font("Mistral", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
-            this.lvNimi3.ForeColor = System.Drawing.Color.White;
-            this.lvNimi3.Location = new System.Drawing.Point(446, 228);
-            this.lvNimi3.Name = "lvNimi3";
-            this.lvNimi3.Size = new System.Drawing.Size(101, 35);
-            this.lvNimi3.TabIndex = 5;
-            this.lvNimi3.Text = "RASMUS";
+            this.lbNimi3.AutoSize = true;
+            this.lbNimi3.Font = new System.Drawing.Font("Mistral", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
+            this.lbNimi3.ForeColor = System.Drawing.Color.White;
+            this.lbNimi3.Location = new System.Drawing.Point(472, 254);
+            this.lbNimi3.Name = "lbNimi3";
+            this.lbNimi3.Size = new System.Drawing.Size(101, 35);
+            this.lbNimi3.TabIndex = 5;
+            this.lbNimi3.Text = "RASMUS";
             // 
             // lbNimi1
             // 
             this.lbNimi1.AutoSize = true;
             this.lbNimi1.Font = new System.Drawing.Font("Mistral", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))));
             this.lbNimi1.ForeColor = System.Drawing.Color.White;
-            this.lbNimi1.Location = new System.Drawing.Point(446, 50);
+            this.lbNimi1.Location = new System.Drawing.Point(472, 76);
             this.lbNimi1.Name = "lbNimi1";
             this.lbNimi1.Size = new System.Drawing.Size(101, 35);
             this.lbNimi1.TabIndex = 6;
             this.lbNimi1.Text = "RASMUS";
+            // 
+            // btPaavalikko
+            // 
+            this.btPaavalikko.BackColor = System.Drawing.Color.LightCoral;
+            this.btPaavalikko.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.btPaavalikko.FlatAppearance.BorderSize = 3;
+            this.btPaavalikko.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btPaavalikko.Font = new System.Drawing.Font("Mistral", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPaavalikko.ForeColor = System.Drawing.Color.Black;
+            this.btPaavalikko.Location = new System.Drawing.Point(660, 412);
+            this.btPaavalikko.Margin = new System.Windows.Forms.Padding(4);
+            this.btPaavalikko.Name = "btPaavalikko";
+            this.btPaavalikko.Size = new System.Drawing.Size(139, 37);
+            this.btPaavalikko.TabIndex = 14;
+            this.btPaavalikko.Text = "PÄÄVALIKKO";
+            this.btPaavalikko.UseVisualStyleBackColor = false;
+            this.btPaavalikko.Click += new System.EventHandler(this.btPaavalikko_Click);
             // 
             // Credits
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.ClientSize = new System.Drawing.Size(838, 488);
+            this.Controls.Add(this.btPaavalikko);
             this.Controls.Add(this.lbNimi1);
-            this.Controls.Add(this.lvNimi3);
+            this.Controls.Add(this.lbNimi3);
             this.Controls.Add(this.lbNimi4);
             this.Controls.Add(this.lbNimi2);
             this.Controls.Add(this.lbTitle);
@@ -141,7 +160,8 @@
         private System.Windows.Forms.Label lbTitle;
         private System.Windows.Forms.Label lbNimi2;
         private System.Windows.Forms.Label lbNimi4;
-        private System.Windows.Forms.Label lvNimi3;
+        private System.Windows.Forms.Label lbNimi3;
         private System.Windows.Forms.Label lbNimi1;
+        private System.Windows.Forms.Button btPaavalikko;
     }
 }
--- Mastermind/View/Credits.cs
+++ Mastermind/View/Credits.cs
@@ -23,7 +23,7 @@ namespace Mastermind
             InitializeComponent();
 
             timer.Tick += new EventHandler(timer_Tick);
-            timer.Interval = (100) * (1);         
+            timer.Interval = (70) * (1);         
             timer.Enabled = true;                      
             timer.Start();                             
 
@@ -36,19 +36,67 @@ namespace Mastermind
 
         void timer_Tick(object sender, EventArgs e)
         {
-            lbSuunnittelu.Location = new Point(lbSuunnittelu.Location.X, lbSuunnittelu.Location.Y - 2);
-            lbToteutus.Location = new Point(lbToteutus.Location.X, lbToteutus.Location.Y - 2);
-
+            lbSuunnittelu.Location = new Point(lbSuunnittelu.Location.X, lbSuunnittelu.Location.Y - 1);
             if (lbSuunnittelu.Location.Y > this.Height)
             {
                 lbSuunnittelu.Location = new Point(lbSuunnittelu.Location.X, 0 - lbSuunnittelu.Height);
             }
 
+            lbToteutus.Location = new Point(lbToteutus.Location.X, lbToteutus.Location.Y - 1);
             if (lbToteutus.Location.Y > this.Height)
             {
                 lbToteutus.Location = new Point(lbToteutus.Location.X, 0 - lbToteutus.Height);
             }
+
+            lbTitle.Location = new Point(lbTitle.Location.X, lbTitle.Location.Y - 1);
+            if (lbTitle.Location.Y > this.Height)
+            {
+                lbTitle.Location = new Point(lbTitle.Location.X, 0 - lbTitle.Height);
+            }
+
+            lbNimi1.Location = new Point(lbNimi1.Location.X, lbNimi1.Location.Y - 1);
+            if (lbNimi1.Location.Y > this.Height)
+            {
+                lbNimi1.Location = new Point(lbNimi1.Location.X, 0 - lbNimi1.Height);
+            }
+
+            lbNimi2.Location = new Point(lbNimi2.Location.X, lbNimi2.Location.Y - 1);
+            if (lbNimi2.Location.Y > this.Height)
+            {
+                lbNimi2.Location = new Point(lbNimi2.Location.X, 0 - lbNimi2.Height);
+            }
+
+            lbNimi3.Location = new Point(lbNimi3.Location.X, lbNimi3.Location.Y - 1);
+            if (lbNimi3.Location.Y > this.Height)
+            {
+                lbNimi3.Location = new Point(lbNimi3.Location.X, 0 - lbNimi3.Height);
+            }
+
+            lbNimi4.Location = new Point(lbNimi4.Location.X, lbNimi4.Location.Y - 1);
+            if (lbNimi4.Location.Y > this.Height)
+            {
+                lbNimi4.Location = new Point(lbNimi4.Location.X, 0 - lbNimi4.Height);
+            }
+
         }
 
+        private void btPaavalikko_Click(object sender, EventArgs e)
+        {
+            Paavalikko paavalikko = ((Paavalikko)Owner);
+
+            //musa.Stop();
+
+            //if (Paavalikko.mute == true)
+            //{
+            //    Paavalikko.menu.PlayLooping();
+            //}
+
+            //else if (Paavalikko.mute == false)
+            //{
+            //    Paavalikko.menu.Stop();
+            //}
+
+            Close();
+        }
     }
 }
